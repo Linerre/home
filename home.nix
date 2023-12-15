0faf91734aa5bfe6fdbe3cbdd8d516751b37a8cc
Linerre <leon.errelin@outlook.com>
2023-12-15 17:08:24 +1100

    Let home-manager take care of some env vars

--- home.nix
+++ home.nix
@@ -61,6 +61,8 @@
   # if you don't want to manage your shell through Home Manager.
   home.sessionVariables = {
     EDITOR = "emacs";
+    PAGER="less";
+
   };
 
   # Let Home Manager install and manage itself.
